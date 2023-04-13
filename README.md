2481c7e496fa5bce7434d7075da5e8617fbe90e3
Raphael <glephunter@gmail.com>
2023-04-13 19:32:11 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # indentmini.nvim
-A minimal and blazing fast indentline plugin
+A minimal and blazing fast indentline plugin by using `nvim_set_decoration_provide` api
+
+## Install
+
+- Lazy.nvim
+
+```lua
+require('lazy').setup({
+    'nvimdev/indentmini.nvim',
+    event = 'BufEnter',
+    config = function()
+        require('indentmini').setup({})
+    end,
+    -- this is no required but if you want indent blanklink line this is needed
+    dependencies = { 'nvim-treesitter/nvim-treesitter'}
+})
+```
+
+## Config
+
+- char    -- string type default is  `│`,
+- exclude -- table  type add exclude filetype in this table
+
+highlight group is `IndentLine`. you can use this to link it to `Comment`
+
+```
+vim.cmd('hi defaule link IndentLine Comment')
+```
+
+## License MIT
---
+++ lua/indentmini/init.lua
@@ -0,0 +1,114 @@
+local api = vim.api
+local treesitter = vim.treesitter
+local nvim_create_autocmd = api.nvim_create_autocmd
+local mini = {}
+
+local function has_treesitter()
+  local ok = pcall(require, 'nvim-treesitter')
+  return ok
+end
+
+local function check_inblock()
+  local type = {
+    'function_definition',
+    'for_statement',
+    'if_statement',
+    'while_statement',
+    'call_expression',
+  }
+  return function(bufnr, row)
+    if not has_treesitter() then
+      return false
+    end
+    local node = treesitter.get_node({ bufnr = bufnr, pos = { row, 0 } })
+    if not node then
+      return false
+    end
+    local parent = node:parent()
+    if parent and vim.tbl_contains(type, parent:type()) then
+      return true
+    end
+    return false
+  end
+end
+
+local function indentline()
+  local ns = api.nvim_create_namespace('IndentLine')
+
+  local function on_win(_, _, bufnr, _)
+    if bufnr ~= vim.api.nvim_get_current_buf() then
+      return false
+    end
+  end
+
+  local ctx = {}
+  local function on_line(_, _, bufnr, row)
+    local indent = vim.fn.indent(row + 1)
+    local text = api.nvim_buf_get_text(bufnr, row, 0, row, -1, {})[1]
+    local inblock = check_inblock()
+    local prev = ctx[#ctx] or 0
+    if indent == 0 and #text == 0 and (prev > 0 or inblock(bufnr, row)) then
+      indent = prev > 20 and 4 or prev
+    end
+
+    ctx[#ctx + 1] = indent
+
+    for i = 1, indent - 1, vim.bo[bufnr].sw do
+      local pos = 'overlay'
+      local symbol = mini.char
+      if #text == 0 and i - 1 > 0 then
+        pos = 'eol'
+        symbol = (i == 1 + vim.bo[bufnr].sw and (' '):rep(vim.bo[bufnr].sw - 1) or '') .. '│'
+      end
+
+      api.nvim_buf_set_extmark(bufnr, ns, row, i - 1, {
+        virt_text = { { symbol, 'IndentLine' } },
+        virt_text_pos = pos,
+        ephemeral = true,
+      })
+    end
+
+    if row + 1 == vim.fn.line('w$') then
+      ctx = {}
+    end
+  end
+
+  local function on_start(_, _)
+    local bufnr = api.nvim_get_current_buf()
+    if
+      vim.bo[bufnr].buftype == 'nofile'
+      or not vim.bo[bufnr].expandtab
+      or vim.tbl_contains(mini.exclude, vim.bo[bufnr].ft)
+    then
+      return false
+    end
+  end
+
+  api.nvim_set_decoration_provider(ns, {
+    on_win = on_win,
+    on_start = on_start,
+    on_line = on_line,
+  })
+end
+
+local function default_exclude()
+  return { 'dashboard', 'lazy', 'help', 'markdown' }
+end
+
+local function setup(opt)
+  mini = vim.tbl_extend('force', {
+    char = '│',
+    exclude = default_exclude(),
+  }, opt or {})
+
+  nvim_create_autocmd('BufEnter', {
+    group = api.nvim_create_augroup('IndentMini', { clear = true }),
+    callback = function()
+      indentline()
+    end,
+  })
+end
+
+return {
+  setup = setup,
+}
