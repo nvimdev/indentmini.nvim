5a82ba8f465704a42d538769977bce940e337f46
Malpha <mega.alpha100@gmail.com>
2023-07-18 14:58:40 +0000

    Fix indentmin causing nvim hang due to access to a buffer local option in a loop
    use `vim.fn.shiftwidth()` in place of `vim.bo[bufnr].sw`

--- lua/indentmini/init.lua
+++ lua/indentmini/init.lua
@@ -34,7 +34,7 @@ local function indentline(hl_current)
 
     ctx[row] = indent
 
-    for i = 1, indent - 1, vim.bo[bufnr].sw do
+    for i = 1, indent - 1, vim.fn.shiftwidth() do
       if col_in_screen(i - 1) then
         local param, col = {}, 0
         if #text == 0 and i - 1 > 0 then
