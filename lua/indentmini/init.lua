1b43e475388ace5d10bdb97edd01bd0b6356c108
Eddie <edward.vetterdrake@alteryx.com>
2024-02-23 11:38:53 -0700

    math?

--- lua/indentmini/init.lua
+++ lua/indentmini/init.lua
@@ -48,8 +48,8 @@ local function indentline()
     for i = 1, indent - 1, shiftw do
       local hi_name = 'IndentLine'
       local iteration = math.floor((i - 1) / shiftw) + 1
-      print(iteration, iteration % 6)
-      local it = iteration % 6
+      local it = iteration % 5
+      print(iteration, it)
       hi_name = string.format('%s%d', hi_name, it == 0 and 1 or it)
 
       if col_in_screen(i - 1) then
